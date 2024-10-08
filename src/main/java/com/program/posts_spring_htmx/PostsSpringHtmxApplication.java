d81db6849842247081517311c6246ed23aa82e8b
nickstrakhov <70016992+nickstrakhov@users.noreply.github.com>
2024-10-08 16:46:39 -0400

    Add files via upload

---
+++ src/main/java/com/program/posts_spring_htmx/PostsSpringHtmxApplication.java
@@ -0,0 +1,13 @@
+package com.program.posts_spring_htmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsSpringHtmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsSpringHtmxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/program/posts_spring_htmx/PostsSpringHtmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.posts_spring_htmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsSpringHtmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
